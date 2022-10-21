80085756e2ba41141edd9bd253f88536d71c5445
Innes <innesoctober@gmail.com>
2022-10-21 16:17:46 +0300

    Дано двузначное число.  Определить:  1. Является ли сумма его цифр двузначным числом  2. Больше ли самого числа сумма его цифр

--- Main.java
+++ Main.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        System.out.println("Введите двузначное число");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+        if (x > 9 && x < 100) {
+            int y = x % 10;
+            System.out.println("вторая цифра " + y);
+            int z = (x - y) / 10;
+            System.out.println("первая цифра " + z);
+            int Sum = y + z;
+            System.out.println("Сумма цифр " + Sum);
+            if (x < Sum) {
+                System.out.println("Сумма цифр больше числа");
+            } else {
+                System.out.println("Сумма цифр меньше числа");
+            }
+        } else {
+            System.out.println("Введено не двузначное число");
+        }
     }
 }

5508f0db6b075f74f47e075f60213f710534f134
Innes <innesoctober@gmail.com>
2022-10-21 16:47:56 +0300

    Даны три вещественных числа a, b, c. Проверить: 1. выполняется ли неравенство a < b < c;  2. выполняется ли неравенство b > a > c.

--- Main.java
+++ Main.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        System.out.println("Введите три вещественных числа");
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        if (a < b && b < c) {
+            System.out.printf("неравенство %.2f<%.2f<%.2f выполняется", a, b, c);
+        } else {
+            System.out.printf("неравенство %.2f<%.2f<%.2f НЕ выполняется", a, b, c);
+        }
+        if (b > a && a > c) {
+            System.out.printf("неравенство %.2f>%.2f>%.2f выполняется", b, a, c);
+        } else {
+            System.out.printf("неравенство %.2f>%.2f>%.2f НЕ выполняется", b, a, c);
+        }
     }
 }

bd304fd7d5764bddfbecdf75be5872c6405db6f2
Innes <innesoctober@gmail.com>
2022-10-21 16:31:23 +0300

    Даны два числа. Если квадратный корень из второго числа меньше первого числа, то увеличить второе число в пять раз.

--- Main.java
+++ Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        System.out.println("Введите первое число");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Введите второе число");
+        int y = sc.nextInt();
+        double x1 = Math.sqrt(x);
+        if (x1 < y) {
+            y = y * 5;
+        }
+
+        System.out.println("Первое число: " + x);
+        System.out.println("Второе число: " + y);
     }
 }

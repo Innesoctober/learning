6e958c619bf9326d1c36716bc32dea4ff9dbae07
Innes <innesoctober@gmail.com>
2022-10-24 15:32:42 +0300

    сумму всех целых чисел от a до b (значения a и в водятся с клавиатуры; b>=a).

--- Main.java
+++ Main.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = sc.nextInt();
+        System.out.println("Введите второе число");
+        int b = sc.nextInt();
+        int sum = 0;
+        for (int z = a; z <= b; z++) {
+            sum = sum + z;
+            System.out.println(sum);
+        }
     }
 }

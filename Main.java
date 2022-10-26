04e930fc95d2dba0c0bd9e3d5fd21f80b721e63b
Innes <innesoctober@gmail.com>
2022-10-26 15:52:54 +0300

    Последовательность Фибоначчи образуется так: первый и второй члены последовательности равны 1, каждый следующий равен сумме двух предыдущих (1, 1, 2, 3, 5, 8, 13, ...). Дано натуральное число n. 1.Найти k-й член последовательности Фибоначчи.  2.Получить первые n членов последовательности Фибоначчи. 3.Верно ли, что сумма первых n членов последовательности Фибоначчи есть четное число?

--- Main.java
+++ Main.java
@@ -2,8 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        System.out.println("Введите число n");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Введите число k");
+        int k = sc.nextInt();
+        int x0 = 1;
+        int x1 = 1;
+        int x2;
+        int sum = 2;
+        String fib = "1 1 ";
+        if (k == 2 || k == 1) {
+            System.out.println("k-ое число последовательности = 1");
+        }
+        for (int i = 3; i <= n; i++) {
+            x2 = x0 + x1;
+            x0 = x1;
+            x1 = x2;
+            fib = fib + x2 + " ";
+            sum = sum + x2;
+            if (i == k) {
+                System.out.println("k-ое число последовательности = " + x2);
+            }
+        }
+        System.out.println("последовательность фибоначчи = " + fib);
+        System.out.println("сумма= " + sum);
+
     }
 }

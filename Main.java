a771f0917a7787dd24aaf2d11c9c1d4bc9f150dc
Innes <innesoctober@gmail.com>
2022-10-28 18:08:55 +0300

    Напечатать числа следующим образом: 25 25.5 24.8 26 26.5 25.8 ... 35 35.5 34.8

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 //        Scanner sc = new Scanner(System.in);
 //        int x = sc.nextInt();
+        for (int i = 25; i <= 35; i++) {
+            System.out.printf("%d %d.5 %d.8\n", i, i, i - 1);
+        }
     }
 }

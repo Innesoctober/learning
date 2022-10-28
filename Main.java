908d5b58a4c0acfaab550ea01b21bd5d5d37eb9b
Innes <innesoctober@gmail.com>
2022-10-28 14:12:33 +0300

    Имеется фрагмент программы в виде оператора цикла с параметром, обеспечивающий вывод на экран "столбиком" всех целых чисел от 10 до 30. Оформить этот фрагмент в виде: 1.оператора цикла с предусловием; 2.оператора цикла с постусловием.

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        int x = 10;
+        /*while (x <= 30) {
+            System.out.println(x);
+            x++;*/
+
+        do {
+            System.out.println(x);
+            x++;
+        }
+        while (x <= 30);
     }
+
 }
+

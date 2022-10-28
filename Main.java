9e460ca7e1d94f9c73d21f50cb2fbd077d9fb243
Innes <innesoctober@gmail.com>
2022-10-28 18:15:10 +0300

    Напечатать таблицу соответствия между весом в фунтах и весом в килограммах для значений 1, 2, ..., 10 фунтов (1 фунт = 453 г)

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 //        Scanner sc = new Scanner(System.in);
 //        int x = sc.nextInt();
+        for (double i = 1; i <= 10; i++) {
+            System.out.printf("%f фунтов соответствует %f килограмм\n", i, i * 435 / 1000);
+        }
     }
 }

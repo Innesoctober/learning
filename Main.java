af1af83de888fdb4a4b405e47534b6e8808877ec
Innes <innesoctober@gmail.com>
2022-10-28 13:57:06 +0300

    Дано натуральное число. Определить: 4. сумму его цифр, больших пяти;

--- Main.java
+++ Main.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число ");
+        int number = sc.nextInt();
+        int counter = 0;
+
+        while (number != 0) {
+            int lastDigit = number % 10;
+            if (lastDigit > 5) {
+                counter++;
+            }
+            number = number / 10;
+        }
+        System.out.println("Количество цифр больше 5: " + counter);
     }
 }
+

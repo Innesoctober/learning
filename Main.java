138ba43b36c8ad80862b75b4afff476401d16ae5
Innes <innesoctober@gmail.com>
2022-10-28 13:48:01 +0300

    Дано натуральное число. Определить: 3. количество четных цифр в нем;

--- Main.java
+++ Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число ");
+        int number = sc.nextInt();
+        int counter = 0;
+
+        while (number != 0) {
+            int lastDigit = number % 10;
+            int chet = lastDigit % 2;
+            if (chet == 0) {
+                counter++;
+            }
+            number = number / 10;
+        }
+        System.out.println("Количество четных цифр: " + counter);
     }
 }
+
+

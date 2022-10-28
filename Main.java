82d2cf35c402a73c26c6f29d9596d87ec79282a3
Innes <innesoctober@gmail.com>
2022-10-28 15:00:42 +0300

    Гражданин  1 марта открыл счет в банке, вложив  1000 руб. Через каждый месяц размер вклада увеличивается на 2% от имеющейся суммы. Определить: 1.за какой месяц величина ежемесячного увеличения вклада превысит 30 руб.; 2.через сколько месяцев размер вклада превысит 1200 руб.

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        double x = 1000;
+        double percent = 0;
+        int month = 3;
+        while (percent <= 30) {
+            percent = percent + 0.02 * x;
+            x += percent;
+            month++;
+        }
+        System.out.println("сумма процентов за " + month + " месяц " + " составила " + percent + " рублей");
+
+        x = 1000;
+        percent = 0;
+        month = 3;
+
+        while (x <= 1200) {
+            percent += 0.02 * x;
+            x += percent;
+            month++;
+        }
+        System.out.println("размер вклада превысит 1200 рублей через  " + (month - 3) + " месяцев");
     }
 }

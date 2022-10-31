45f33fc3532f5a37dcea224e23abd6659c538668
Innes <innesoctober@gmail.com>
2022-10-31 16:19:16 +0300

    Реализовать функцию, которая принимает на вход два числа n и m и возвращает n в степени m. Реализовать перегрузку для разных типлв данных.

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
+    public static double Stepen(int n, int m) {
+        return (Math.pow(n, m));
+    }
+
+    public static double Stepen(double n, int m) {
+        return (Math.pow(n, m));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+
+        System.out.println(Stepen(5.6, 5));
     }
 }
+
+

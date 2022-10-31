d3b0adf2bb6caaaf24d605849925f4f1990ca17c
Innes <innesoctober@gmail.com>
2022-10-31 16:07:00 +0300

    Имеется зарплата сотрудника-натуральное число. реализовать функцию, которая рассчитывает премию сотрудника-вещественное число, исходя из таблицы: <40000  50% премия 40000<=x<=80000 70% >=80000 110%

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
+    public static double Premia(int x) {
+        if (x < 40000)
+            return (x / 50);
+
+        if ((x >= 40000) || (x <= 80000))
+            return (x / 100 * 70);
+
+        if (x >= 80000)
+            return (x / 100 * 110);
+        else
+            return 0;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        System.out.println("Зарплата: ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(Premia(x));
     }
 }

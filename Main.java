1d90ef9a903d618c1532fcf74b1b75dd0c001f57
Innes <innesoctober@gmail.com>
2022-10-31 15:53:02 +0300

    Написать метод, который принимает на вход натуральное число и возвращает true, если число-четное

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    public static boolean chet(int x) {
+
+        if (x % 2 == 0) {
+            return (true);
+        } else {
+            return (false);
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+        System.out.println("Введите число");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(chet(x));
     }
 }

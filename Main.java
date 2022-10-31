3480f18340b4ceb982d6cb4490d63d173f1ee851
Innes <innesoctober@gmail.com>
2022-10-31 15:23:46 +0300

    Реализовать функцию нахождения минимума: 1.Из трёх чисел 2.Из четырёх чисел

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
-import java.util.Scanner;
-
 public class Main {
+    public static int min(int a, int b) {
+        if (a < b)
+            return a;
+        else
+            return b;
+    }
+
+    public static int min(int a, int b, int c, int d) {
+        return min(min(a, b), min(c, d));
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 //        Scanner sc = new Scanner(System.in);
 //        int x = sc.nextInt();
+        System.out.println(min(10, 15, 5, 14));
     }
 }

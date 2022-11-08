632235f7096edd3f8a6104d48700215f71e69e24
Innes <innesoctober@gmail.com>
2022-11-08 20:07:23 +0300

    Change

--- Main.java
+++ Main.java
@@ -1,9 +1,40 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    public static void printArray(double[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%.2f ", array[i]);
+        }
+        System.out.println();
+    }
+
+    public static double[] scanArray(int n) {
+
+        double[] array = new double[n];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextDouble();
+        }
+
+        return array;
+    }
+
+    public static double[] generateArray(int n) {
+
+        double[] array = new double[n];
+        Random r = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextDouble() * 100;
+        }
+
+        return array;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        Scanner sc = new Scanner(System.in);
-//        int x = sc.nextInt();
+
     }
 }

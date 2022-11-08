ac9b7c110f3cd05d32777c89218e2e588cb3818d
Innes <innesoctober@gmail.com>
2022-11-08 20:45:41 +0300

    урок 6 задача 7

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+/*
+Дан массив.
+ Поменять местами:
+  1.второй и пятый элементы;
+  2.третий и максимальный элементы.
+   Если элементов с максимальным значением несколько,
+   то в обмене должен участвовать первый из них.
+ */
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -34,7 +43,63 @@ public class Main {
         return array;
     }
 
-    public static void main(String[] args) {
+    public static double[] swapElementsOfArray(double[] array, int n, int m) {
 
+        if ((n < 0) || (n >= array.length) || (m < 0) || (m >= array.length)) {
+            return array;
+        } else {
+            double temp = array[n];
+            array[n] = array[m];
+            array[m] = temp;
+
+            return array;
+        }
+    }
+
+    public static double findMaxElementOfArray(double[] array) {
+
+        double max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static int findIndexOfMaxElementOfArray(double[] array) {
+
+        int index = 0;
+
+        double max = findMaxElementOfArray(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == max) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Введите длину массива");
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        System.out.println("Введите элементы массива");
+        double[] array = scanArray(length);
+        System.out.println("Исходный массив");
+        printArray(array);
+        swapElementsOfArray(array, 2, 5);
+        System.out.println("Поменяли 2 и 5 элементы местами");
+        printArray(array);
+        int maxIndex = findIndexOfMaxElementOfArray(array);
+        System.out.println("Индекс максимального элемента массива: " + maxIndex);
+        swapElementsOfArray(array, 3, maxIndex);
+        System.out.println("Поменяли 3 и максимальный элементы местами");
+        printArray(array);
     }
 }
